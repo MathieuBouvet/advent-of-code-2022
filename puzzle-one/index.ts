b32b73a3b07e29ac65eed4e51f067f0a73d0eaf4
Mathieu Bouvet <mathieubouvet42@gmail.com>
2022-12-03 16:34:37 +0100

    solve puzzle one

---
+++ puzzle-one/index.ts
@@ -0,0 +1,19 @@
+import { readFile } from "fs/promises";
+
+const input = await readFile("./puzzle-one/input.txt", { encoding: "utf-8" });
+
+const inputByElf = input.split("\n\n");
+
+const parsedByElf = inputByElf
+  .map(rawInputs => rawInputs.split("\n").map(rawInput => Number(rawInput)));
+
+const elfCalories = parsedByElf.map(elfCalories => elfCalories.reduce((total, colory) => {
+  return total + colory
+}, 0))
+
+
+const sortedCalories = [...elfCalories].sort((a, b) => b - a);
+
+const topThree = [sortedCalories[0], sortedCalories[1], sortedCalories[2]];
+
+console.log(sortedCalories[0] + sortedCalories[1] + sortedCalories[2])
